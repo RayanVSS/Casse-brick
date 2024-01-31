18ed48a1a599fac13743570f00ae99e77d7ff589
Majda <majda.benmalek@etu.u-paris.fr>
2024-01-31 23:51:35 +0100

    #5 quelque piste de reflexion mais rien de concret encore

--- src/main/java/entite/Entite.java
+++ src/main/java/entite/Entite.java
@@ -1,12 +1,2 @@
-package entite;
-
-import geometry.*;
-
-public abstract class Entite {
-
-    public abstract Coordonnee getC();
-    
-    public abstract void setType(int type);
-
-    public abstract int getType();
+package entite;public class Entite {
 }
