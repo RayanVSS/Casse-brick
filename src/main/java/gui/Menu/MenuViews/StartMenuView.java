b9312cb3ce144317275cff7bb9d38847a996604c
Majda <majda.benmalek@etu.u-paris.fr>
2024-02-11 18:38:09 +0100

    #10 button tuto ajouté (qui vas pttr servir a expliqué le jeu et les controle (ou pas))

--- src/main/java/gui/Menu/MenuViews/StartMenuView.java
+++ src/main/java/gui/Menu/MenuViews/StartMenuView.java
@@ -6,23 +6,26 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import utils.GameConstants;
 
-public class StartMenuView implements Menu{
+public class StartMenuView implements Menu {
     private Stage primaryStage;
     private Scene scene;
     private StackPane root;
     private Button btnPlay;
     private Button btnOptions;
     private Button btnQuit;
+    private Button btnTuto;
 
     public StartMenuView(Stage p) {
         this.primaryStage = p;
         this.root = new StackPane();
         this.scene = new Scene(root, GameConstants.DEFAULT_WINDOW_WIDTH, GameConstants.DEFAULT_WINDOW_HEIGHT);
         this.root.setStyle("-fx-background-color: #273654;");
-        this.btnPlay = createButton("Jouer", 0, 250);
-        this.btnOptions = createButton("Options", 0, 150);
-        this.btnQuit = createButton("Quitter", 0, 50);
-        root.getChildren().addAll(btnPlay, btnOptions, btnQuit);
+        this.btnPlay = createButton("Jouer", 0, 300);
+        this.btnOptions = createButton("Options", 0, 200);
+        this.btnTuto = createButton("Tutoriel", 0, 100);
+        this.btnQuit = createButton("Quitter", 0, 0);
+
+        root.getChildren().addAll(btnPlay, btnOptions, btnQuit, btnTuto);
         primaryStage.setScene(scene);
     }
 
@@ -30,17 +33,24 @@ public class StartMenuView implements Menu{
     public StackPane getRoot() {
         return root;
     }
+
     public Stage getPrimaryStage() {
         return primaryStage;
     }
+
     public Button getBtnPlay() {
         return btnPlay;
     }
+
     public Button getBtnOptions() {
         return btnOptions;
     }
+
     public Button getBtnQuit() {
         return btnQuit;
     }
-}
 
+    public Button getBtnTuto() {
+        return btnTuto;
+    }
+}
