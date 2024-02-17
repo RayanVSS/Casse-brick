f0b15d8fabe413d64315eb9199e02715215f5d04
Amenah <amenah.mushtaq@etu.u-paris.fr>
2024-02-17 17:50:57 +0100

    factorisation

--- src/main/java/gui/BricksGraphics.java
+++ src/main/java/gui/BricksGraphics.java
@@ -1,7 +1,6 @@
 package gui;
 
 import entity.brick.Brick;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
@@ -14,7 +13,13 @@ public class BricksGraphics extends StackPane {
     public BricksGraphics(Image image, Brick brique) {
         this.brique = brique;
         this.imageView = new ImageView(image);
-        // this.imageView.setPreserveRatio(true);
+        this.imageView.setFitWidth(GameConstants.BRICK_WIDTH);
+        this.imageView.setFitHeight(GameConstants.BRICK_HEIGHT);
+        getChildren().add(this.imageView);
+    }
+
+    public void setImageView(Image image) {
+        this.imageView = new ImageView(image);
         this.imageView.setFitWidth(GameConstants.BRICK_WIDTH);
         this.imageView.setFitHeight(GameConstants.BRICK_HEIGHT);
         getChildren().add(this.imageView);
@@ -23,33 +28,17 @@ public class BricksGraphics extends StackPane {
     public void update() {
         if (brique != null) {
             if (brique.isDestroyed()) {
-                // System.out.println("CASSE");
                 getChildren().remove(imageView);
                 imageView = new ImageView();
                 getChildren().add(imageView);
             } else if (brique.getDurability() < 70 && brique.getDurability() > 50) {
                 getChildren().remove(imageView);
-                // System.out.println("---------------------------");
-                // System.out.println("1");
-                // System.out.println("---------------------------");
                 Image im = ImageLoader.loadImage("src/main/ressources/briquec.png");
-                imageView = new ImageView(im);
-                // imageView.setPreserveRatio(true);
-                imageView.setFitWidth(GameConstants.BRICK_WIDTH);
-                imageView.setFitHeight(GameConstants.BRICK_HEIGHT);
-                getChildren().add(imageView);
+                this.setImageView(im);
             } else if (brique.getDurability() > 0 && brique.getDurability() < 50) {
                 getChildren().remove(imageView);
-                // System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                // System.out.println("2");
-                // System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 Image im = ImageLoader.loadImage("src/main/ressources/briquecc.png");
-                imageView = new ImageView(im);
-                // imageView.setPreserveRatio(true);
-                imageView.setFitWidth(GameConstants.BRICK_WIDTH);
-                imageView.setFitHeight(GameConstants.BRICK_HEIGHT);
-                // imageView.setFitWidth(60);
-                getChildren().add(this.imageView);
+                this.setImageView(im);
             }
         }
     }
