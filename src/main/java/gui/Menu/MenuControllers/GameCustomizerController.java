afe20533f4a168ce3939166a7ec19f5a58df63c6
Olivier GUAN <OlivierGuan99@gmail.com>
2024-03-04 12:46:12 +0100

    Save

--- src/main/java/gui/Menu/MenuControllers/GameCustomizerController.java
+++ src/main/java/gui/Menu/MenuControllers/GameCustomizerController.java
@@ -1,5 +1,12 @@
 package gui.Menu.MenuControllers;
 
+import gui.Menu.MenuViews.GameCustomizerView;
+
 public class GameCustomizerController {
 
+    private GameCustomizerView gameCustomizerView;
+
+    public GameCustomizerController(GameCustomizerView gameCustomizerView) {
+        this.gameCustomizerView = gameCustomizerView;
+    }
 }
--- src/main/java/gui/Menu/MenuViews/GameCustomizerView.java
+++ src/main/java/gui/Menu/MenuViews/GameCustomizerView.java
@@ -1,5 +1,27 @@
 package gui.Menu.MenuViews;
 
+import gui.Menu.MenuControllers.GameCustomizerController;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import utils.GameConstants;
+
 public class GameCustomizerView implements Menu {
 
+    private Stage primaryStage;
+    private HBox root;
+    private Scene scene;
+
+    private Button backButton;
+
+    public GameCustomizerView(Stage primaryStage) {
+
+        this.primaryStage = primaryStage;
+        root = new HBox(45);
+        scene = new Scene(root, GameConstants.DEFAULT_WINDOW_WIDTH, GameConstants.DEFAULT_WINDOW_HEIGHT);
+
+        backButton = createButton("Retour", 0, 0);
+        new GameCustomizerController(this);
+    }
 }
