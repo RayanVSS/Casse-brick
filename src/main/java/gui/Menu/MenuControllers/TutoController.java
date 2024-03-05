7a54c84db512c404242c6f08c70b8d2c2f7990ee
Majdaaaaa <majda.benmalek@etu.u-paris.fr>
2024-03-05 10:41:54 +0100

    branch

--- src/main/java/gui/Menu/MenuControllers/TutoController.java
+++ src/main/java/gui/Menu/MenuControllers/TutoController.java
@@ -3,11 +3,14 @@ package gui.Menu.MenuControllers;
 import gui.App;
 import gui.Menu.MenuViews.TutoView;
 import javafx.application.Platform;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 /**
  * Classe TutoController qui gère les interactions de l'utilisateur avec la vue
  * TutoView.
+ * 
  * @see TutoView
  * @author Benmalek Majda
  * 
@@ -20,9 +23,10 @@ public class TutoController {
      * 
      * @param p Le stage principal sur lequel la vue du tutoriel est affichée.
      */
-    public TutoController(Stage p,TutoView view) { 
+    public TutoController(Stage p, TutoView view) {
         this.view = view;
         this.view.getBtnBack().setOnAction(e -> back());
+        this.view.getMusique().setOnAction(e -> music());
     }
 
     /**
@@ -32,5 +36,16 @@ public class TutoController {
         Platform.runLater(() -> {
             App.sceneManager.changeScene(view.getPrimaryStage(), "StartMenuView");
         });
+    }
+
+    private void music() {
+        try {
+            Media sound = new Media(getClass().getResource("/music/test2.mp3").toExternalForm());
+            MediaPlayer mediaPlayer = new MediaPlayer(sound);
+            mediaPlayer.play();
+            System.out.println("musique");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/gui/Menu/MenuViews/TutoView.java
+++ src/main/java/gui/Menu/MenuViews/TutoView.java
@@ -5,7 +5,6 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import utils.GameConstants;
 import gui.Menu.Menu;
-import gui.Menu.SceneManager;
 import gui.Menu.MenuControllers.TutoController;
 import javafx.scene.Scene;
 
@@ -13,16 +12,18 @@ public class TutoView implements Menu{
     private Stage primaryStage;
     private static StackPane root=new StackPane();
     private static Scene scene= new Scene(root, GameConstants.DEFAULT_WINDOW_WIDTH, GameConstants.DEFAULT_WINDOW_HEIGHT);
-    private SceneManager sceneManager;
     private Button btnBack;
+    private Button musique;
     
     public TutoView(Stage p) {
         this.primaryStage = p;
         root = new StackPane();
         scene = new Scene(root, GameConstants.DEFAULT_WINDOW_WIDTH, GameConstants.DEFAULT_WINDOW_HEIGHT);
         root.getStyleClass().add("root");
-        this.btnBack = createButton("Retour", 0, 0);
+        this.btnBack = createButton("Retour", 0, 200);
+        this.musique=createButton("musique", 0, 100);
         root.getChildren().add(btnBack);
+        root.getChildren().add(musique);
         new TutoController(p,this);
     }
     public Button getBtnBack() {
@@ -37,7 +38,7 @@ public class TutoView implements Menu{
     public Scene getScene() {
         return scene;
     }
-    public SceneManager getSceneManager() {
-        return sceneManager;
+    public Button getMusique(){
+        return musique;
     }
 }
