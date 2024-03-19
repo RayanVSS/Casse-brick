80e7d71831ba253941bf62781af38955b7d0f464
Majdaaaaa <majda.benmalek@etu.u-paris.fr>
2024-03-19 11:15:07 +0100

    modif de la taille des images

--- src/main/java/gui/GraphicsFactory/BallGraphics.java
+++ src/main/java/gui/GraphicsFactory/BallGraphics.java
@@ -12,7 +12,7 @@ import javafx.scene.image.ImageView;
 public class BallGraphics extends ImageView {
 
     private Ball ball;
-    private Image classicBall = new Image("/balle/balle1.png");
+    private Image classicBall = new Image("/balle/balleGravity.png");
     private Image hyperBall = new Image("/balle/balleHyper.png");
     private Image gravityBall = new Image("/balle/balleGravity.png");
     private Image positifBall = new Image("/balle/positif.png");
@@ -35,34 +35,60 @@ public class BallGraphics extends ImageView {
     }
 
     private void updateImageAndProperties() {
-        if (ball instanceof ClassicBall)
+        if (ball instanceof ClassicBall) {
             setImage(classicBall);
-        else if (ball instanceof HyperBall)
+            setFitWidth(ball.getRadius() * 3);
+            setFitHeight(ball.getRadius() * 3);
+            ball.setRadius(ball.getRadius() * 3);
+        } else if (ball instanceof HyperBall) {
             setImage(hyperBall);
-        else if (ball instanceof GravityBall)
+            setFitWidth(ball.getRadius() * 3);
+            setFitHeight(ball.getRadius() * 3);
+            ball.setRadius(ball.getRadius() * 3);
+        } else if (ball instanceof GravityBall) {
             setImage(gravityBall);
-        else if (ball instanceof MagnetBall) {
-            if (((MagnetBall) ball).getEtat().equals("positif"))
+            setFitWidth(ball.getRadius() * 3);
+            setFitHeight(ball.getRadius() * 3);
+            ball.setRadius(ball.getRadius() * 3);
+        } else if (ball instanceof MagnetBall) {
+            if (((MagnetBall) ball).getEtat().equals("positif")) {
                 setImage(positifBall);
-            else
+                setFitWidth(ball.getRadius() * 3);
+                setFitHeight(ball.getRadius() * 3);
+                ball.setRadius(ball.getRadius() * 3);
+            } else {
                 setImage(negatifBall);
+                setFitWidth(ball.getRadius() * 3);
+                setFitHeight(ball.getRadius() * 3);
+                ball.setRadius(ball.getRadius() * 3);
+            }
         }
         if (ball.getColor() != null) {
             switch (ball.getColor()) {
                 case RED:
                     setImage(redBall);
+                    setFitWidth(ball.getRadius() * 4);
+                    setFitHeight(ball.getRadius() * 4 );
+                    ball.setRadius(ball.getRadius() * 4);
                     break;
                 case GREEN:
                     setImage(greenBall);
+                    setFitWidth(ball.getRadius() * 4);
+                    setFitHeight(ball.getRadius() * 4);
+                    ball.setRadius(ball.getRadius() * 4);
                     break;
                 case BLUE:
                     setImage(blueBall);
+                    setFitWidth(ball.getRadius() * 4);
+                    setFitHeight(ball.getRadius() * 4);
+                    //ball.setRadius(ball.getRadius() * 4);
                     break;
             }
         }
-        setFitWidth(ball.getRadius() * 3);
-        setFitHeight(ball.getRadius() * 3);
+        // setFitWidth(ball.getRadius() * 3);
+        // setFitHeight(ball.getRadius() * 3);
         setPreserveRatio(true);
-        setCache(true);
+        setSmooth(true);
+        // setCache(true);
     }
 }
