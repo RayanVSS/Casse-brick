c8e223587be53411fe66ae1991a26ce262a29ce1
belhasse <rayan.belhassen@etu.u-paris.fr>
2024-04-20 20:13:19 +0200

    test

--- src/main/java/gui/GraphicsFactory/RacketGraphics.java
+++ src/main/java/gui/GraphicsFactory/RacketGraphics.java
@@ -5,8 +5,16 @@ import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
+import physics.entity.*;
 import entity.racket.*;
 import physics.entity.Racket;
+import utils.GameConstants;
+import gui.ImageLoader;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import java.io.File;
+
+import config.Game;
 
 /**
  * Classe RacketGraphics qui encapsule un objet Shape pour représenter graphiquement une raquette.
@@ -22,6 +30,9 @@ public class RacketGraphics {
         this.racket = racket;
         this.shapeType = shapeType;
         setShape();
+        if(!GameConstants.TEXTURE.equals("Null")){
+            addTexture();
+        }
     }
 
     private void setShape() {
@@ -63,13 +74,36 @@ public class RacketGraphics {
                 throw new IllegalArgumentException("Forme non reconnue: " + shapeType);
         }
 
-        if (racket instanceof ClassicRacket)
-            shape.getStyleClass().add("racket");
-        else if (racket instanceof YNotFixeRacket)
-            shape.getStyleClass().add("ynotfixeracket");
-        else if (racket instanceof MagnetRacket)
-            shape.getStyleClass().add("magnetracket");
+        if (GameConstants.TEXTURE.equals("Null")){
+            if (racket instanceof ClassicRacket){
+                shape.getStyleClass().add("racket");
+            }   
+            else if (racket instanceof YNotFixeRacket){
+                shape.getStyleClass().add("ynotfixeracket");
+            }
+            else if (racket instanceof MagnetRacket){
+                shape.getStyleClass().add("magnetracket");
+            } 
+        } else {
+            addTexture();
+        }
     }
+
+    private void addTexture() {
+        String texturePath = "src/main/ressources/Texture/" + GameConstants.TEXTURE; // Chemin vers votre texture
+        // Vérification si le fichier de texture existe
+        File textureFile = new File(texturePath);
+        if (!textureFile.exists()) {
+            throw new IllegalArgumentException("Fichier de texture introuvable: " + texturePath);
+        }
+
+        ImagePattern texture = new ImagePattern(new Image(textureFile.toURI().toString()));
+
+        // Remplissage de la forme de la raquette avec la texture
+        shape.setFill(texture);
+        shape.setStroke(texture);
+    }
+
 
     public Shape getShape() {
         return shape;
