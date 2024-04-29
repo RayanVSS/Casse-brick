23504e5b6de018c23dca314cb087b15f365154aa
Olivier GUAN <olivierguan99@gmail.com>
2024-04-29 23:45:19 +0200

    Petite correction demoEnv

--- src/main/java/physics/config/GamePhysics.java
+++ src/main/java/physics/config/GamePhysics.java
@@ -58,6 +58,8 @@ public class GamePhysics {
             if (!ball.movement()) {
                 // ball.getC().setX(-ball.getC().getX());
                 // ball.getC().setY(-ball.getC().getY());
+                ball.getDirection().setX(-ball.getC().getX());
+                ball.getDirection().setY(-ball.getC().getY());
             }
         }
     }
